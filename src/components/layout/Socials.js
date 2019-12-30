595a7a127c6699ac1eb81d9e2670e55da14c6e4e
Derek Lause <derekmlause@gmail.com>
2019-12-30 15:09:51 -0600

    a few more things :)

--- src/components/layout/Socials.js
+++ src/components/layout/Socials.js
@@ -14,10 +14,10 @@ const Socials = () => {
                 </Row>
                 <Row>
                     <Col md={{span: 4, offset: 4}}>
-                        <FontAwesomeIcon icon={faTwitter} href="" title="Derek Lause's Twitter"/>
-                        <FontAwesomeIcon icon={faGithub} href="" title="Derek Lause's Github"/>
-                        <FontAwesomeIcon icon={faLinkedin} href="" title="Derek Lause's LinkedIn"/>
-                        <FontAwesomeIcon icon={faYoutube} href="" title="Derek Lause's Youtube"/>
+                        <a href="https://twitter.com/dmlause"><FontAwesomeIcon icon={faTwitter}  title="Derek Lause's Twitter"/></a>
+                        <a href="https://github.com/dmlause"><FontAwesomeIcon icon={faGithub}  title="Derek Lause's Github"/></a>
+                        <a href="https://www.linkedin.com/in/dereklause"><FontAwesomeIcon icon={faLinkedin}  title="Derek Lause's LinkedIn"/></a>
+                        <a href="https://www.youtube.com/channel/UCNoAX-XqNBMlP7CkEYgewVg"><FontAwesomeIcon icon={faYoutube}  title="Derek Lause's Youtube"/></a>
                     </Col>
                 </Row>
             </div>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -14,7 +14,7 @@ class About extends React.Component {
                         <div>
                             <h2>Hi, I'm Derek Lause.</h2>
                             <p>I am a self-motivated full stack software engineer, as well as a leader and mentor.</p>
-                            <p>My <a href='/portfolio' className="about-link">portfolio page</a> describes my skillset and accomplishments</p>
+                            <p>You're welcome to see my skillset and accomplishments on the <a href='/portfolio' className="about-link">portfolio page</a>.</p>
                             <p>Check out my <a href='https://dlause.blogspot.com' className="about-link">blog content</a> about everything technology</p>
                             <Button className="about-button" href="/contact" title="Contact Derek Lause">
                                 Contact Derek <FontAwesomeIcon icon={faPaperPlane}/>
